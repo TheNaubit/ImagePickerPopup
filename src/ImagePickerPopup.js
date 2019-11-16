a0d9dc13467f927fa6f01945eabec52f117fc986
Alberto <nautadev@gmail.com>
2019-11-16 22:42:03 +0100

    More fiex.

--- src/ImagePickerPopup.js
+++ src/ImagePickerPopup.js
@@ -261,7 +261,7 @@ class ImagePickerPopup {
     // They are stored in the main div (this.imagePopupWindow)
     // In a data-path array
     this.images = JSON.parse(this.imagePopupWindow.getAttribute("data-path"));
-    if (this.images === undefined || this.images === null || this.images.length <= 0) {
+    if (this.images === undefined || this.images === null || (this.images instanceof Array) === false || this.images.length <= 0) {
       if(this.debug){
         console.log("No initial images loaded!");
       }
@@ -273,7 +273,7 @@ class ImagePickerPopup {
     // So we have to get them (they are stored in the main div in a data-alt array)
     // It is optional, so maybe it doesn't exist
     this.altImages = JSON.parse(this.imagePopupWindow.getAttribute("data-alt"));
-    if (this.altImages === undefined || this.altImages === null || this.altImages.length <= 0) {
+    if (this.altImages === undefined || this.altImages === null || (this.altImages instanceof Array)===false || this.altImages.length <= 0) {
       if(this.debug){
         console.log("No initial alt images loaded!");
       }
